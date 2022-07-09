91efdb161264ef03e9204bade394e74008448cae
Ankit Kathayat <93024110+iamankitkathayat@users.noreply.github.com>
2022-07-09 23:48:38 +0530

    Update Restaurant.js

--- src/component/Basics/Restaurant.js
+++ src/component/Basics/Restaurant.js
@@ -28,3 +28,16 @@ const Resturant = () => {
     const updatedList = Menu.filter((curElem) => {
       return curElem.category === category;
     });
+
+    setMenuData(updatedList);
+  };
+
+  return (
+    <>
+      <Navbar filterItem={filterItem} menuList={menuList} />
+      <MenuCard menuData={menuData} />
+    </>
+  );
+};
+
+export default Resturant;
