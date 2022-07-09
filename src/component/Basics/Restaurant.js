463738ca8b5e7c0e7e6493ae038b63bd74d703c3
Ankit Kathayat <93024110+iamankitkathayat@users.noreply.github.com>
2022-07-09 23:48:22 +0530

    Update Restaurant.js

--- src/component/Basics/Restaurant.js
+++ src/component/Basics/Restaurant.js
@@ -18,3 +18,13 @@ console.log(uniqueList);
 const Resturant = () => {
   const [menuData, setMenuData] = useState(Menu);
   const [menuList, setMenuList] = useState(uniqueList);
+
+  const filterItem = (category) => {
+    if (category === "All") {
+      setMenuData(Menu);
+      return;
+    }
+
+    const updatedList = Menu.filter((curElem) => {
+      return curElem.category === category;
+    });
