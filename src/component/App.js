730799bf9ce8e1885ac90f3f6022ae52404b3221
Ankit Kathayat <93024110+iamankitkathayat@users.noreply.github.com>
2022-07-10 23:05:53 +0530

    Update App.js

--- src/component/App.js
+++ src/component/App.js
@@ -1,1 +1,5 @@
-
+import React from "react";
+// import Resturant from "./component/Basics/Resturant";cd 
+// import UseState from "./component/Hooks/useState";
+// import UseEffect from "./component/Hooks/useEffect";
+import UseReducer from "./component/Hooks/useReducer";
