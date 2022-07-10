fca0f2891d988256dd58c11b2ed4a7a8a3d70f79
Ankit Kathayat <93024110+iamankitkathayat@users.noreply.github.com>
2022-07-10 23:06:12 +0530

    Update App.js

--- src/component/App.js
+++ src/component/App.js
@@ -3,3 +3,9 @@ import React from "react";
 // import UseState from "./component/Hooks/useState";
 // import UseEffect from "./component/Hooks/useEffect";
 import UseReducer from "./component/Hooks/useReducer";
+
+const App = () => {
+  return <UseReducer />;
+};
+
+export default App;
