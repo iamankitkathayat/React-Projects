ac9fd71e0c7daab9590a09d99733abc70e07f256
Ankit Kathayat <93024110+iamankitkathayat@users.noreply.github.com>
2022-07-20 23:31:54 +0530

    Update menuApi.js

--- src/component/Basics/menuApi.js
+++ src/component/Basics/menuApi.js
@@ -3,3 +3,8 @@ const Menu = [
     id: 1,
     image: "images/maggi.jpg",
     name: "maggi",
+    category: "breakfast",
+    price: "12₹",
+    description:
+      "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
