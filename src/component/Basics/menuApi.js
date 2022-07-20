61430ef295858336b9d411e1a9f8662b5ecc74fb
Ankit Kathayat <93024110+iamankitkathayat@users.noreply.github.com>
2022-07-20 23:32:30 +0530

    Update menuApi.js

--- src/component/Basics/menuApi.js
+++ src/component/Basics/menuApi.js
@@ -18,3 +18,8 @@ const Menu = [
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
   },
+  {
+    id: 3,
+    image: "images/corn.jpg",
+    name: "corn",
+    category: "breakfast",
