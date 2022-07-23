6912025eb6898262d894313449cc09d3d41ed99a
Ankit Kathayat <93024110+iamankitkathayat@users.noreply.github.com>
2022-07-23 22:55:33 +0530

    Update useEffect.js

--- src/component/Hooks/useEffect.js
+++ src/component/Hooks/useEffect.js
@@ -2,3 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./style.css";
 
 const UseEffect = () => {
+  // const initialData = 15;
+  const [myNum, setMyNum] = useState(0);
+
+  useEffect(() => {
