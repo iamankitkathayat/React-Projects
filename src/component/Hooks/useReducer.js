0586210542ea33cafac60ff58cd54cdbfb14b2e3
Ankit Kathayat <93024110+iamankitkathayat@users.noreply.github.com>
2022-07-24 23:12:05 +0530

    Update useReducer.js

--- src/component/Hooks/useReducer.js
+++ src/component/Hooks/useReducer.js
@@ -27,3 +27,9 @@ const UseReducer = () => {
           <span></span>
           <span></span>
           <span></span>
+          INCR
+        </div>
+        <div class="button2" onClick={() => dispatch({ type: "DECR" })}>
+          <span></span>
+          <span></span>
+          <span></span>
