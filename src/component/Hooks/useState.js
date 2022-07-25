013e701dde0c49640f5111fea6548593e33f84e5
Ankit Kathayat <93024110+iamankitkathayat@users.noreply.github.com>
2022-07-25 23:53:35 +0530

    Update useState.js

--- src/component/Hooks/useState.js
+++ src/component/Hooks/useState.js
@@ -6,3 +6,7 @@ const UseState = () => {
   const [myNum, setMyNum] = React.useState(0);
 
   return (
+    <>
+      <div className="center_div">
+        <p>{myNum}</p>
+        <div class="button2" onClick={() => setMyNum(myNum + 1)}>
