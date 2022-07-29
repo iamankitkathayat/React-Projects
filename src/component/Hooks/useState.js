0f1c78796a85c411bd701cba4be61e9641deecb8
Ankit Kathayat <93024110+iamankitkathayat@users.noreply.github.com>
2022-07-29 23:52:48 +0530

    Update useState.js

--- src/component/Hooks/useState.js
+++ src/component/Hooks/useState.js
@@ -16,3 +16,11 @@ const UseState = () => {
           <span></span>
           INCR
         </div>
+        <div
+          class="button2"
+          onClick={() => (myNum > 0 ? setMyNum(myNum - 1) : setMyNum(0))}>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          DECR
