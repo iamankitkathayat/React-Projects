5167b7647ef855f2fc08f1c94d50fc43a1742e02
Ankit Kathayat <93024110+iamankitkathayat@users.noreply.github.com>
2022-07-29 23:52:37 +0530

    Update useState.js

--- src/component/Hooks/useState.js
+++ src/component/Hooks/useState.js
@@ -10,3 +10,9 @@ const UseState = () => {
       <div className="center_div">
         <p>{myNum}</p>
         <div class="button2" onClick={() => setMyNum(myNum + 1)}>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          INCR
+        </div>
