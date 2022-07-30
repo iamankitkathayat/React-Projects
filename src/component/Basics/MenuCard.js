6714f91517bd462b5f36eaddc6d62a7b76b7a1d0
Ankit Kathayat <93024110+iamankitkathayat@users.noreply.github.com>
2022-07-30 23:49:09 +0530

    Update MenuCard.js

--- src/component/Basics/MenuCard.js
+++ src/component/Basics/MenuCard.js
@@ -18,3 +18,10 @@ const MenuCard = ({ menuData }) => {
                     <span className="card-author subtle"> {category}</span>
                     <h2 className="card-title"> {name} </h2>
                     <span className="card-description subtle">
+                      {description}
+                    </span>
+                    <div className="card-read">Read</div>
+                  </div>
+                  <img src={image} alt="images" className="card-media" />
+
+                  <span className="card-tag  subtle">Order Now</span>
