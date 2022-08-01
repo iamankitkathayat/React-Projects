f134191c475d5ed093ee9062e3bfd874471a718d
Ankit Kathayat <93024110+iamankitkathayat@users.noreply.github.com>
2022-08-01 23:51:19 +0530

    Update useState.js

--- src/component/Hooks/useState.js
+++ src/component/Hooks/useState.js
@@ -3,3 +3,8 @@ import "./style.css";
 
 const UseState = () => {
   // const initialData = 15;
+  const [myNum, setMyNum] = React.useState(0);
+
+  return (
+    <>
+      <div className="center_div">
