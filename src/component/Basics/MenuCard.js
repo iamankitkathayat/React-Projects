8e57165ee3b3e7a888b5807b7de34af0d05bf40c
Ankit Kathayat <93024110+iamankitkathayat@users.noreply.github.com>
2022-08-02 23:56:35 +0530

    Update MenuCard.js

--- src/component/Basics/MenuCard.js
+++ src/component/Basics/MenuCard.js
@@ -12,3 +12,10 @@ const MenuCard = ({ menuData }) => {
           return (
             <>
               <div className="card-container" key={id}>
+                <div className="card ">
+                  <div className="card-body">
+                    <span className="card-number card-circle subtle">{id}</span>
+                    <span className="card-author subtle"> {category}</span>
+                    <h2 className="card-title"> {name} </h2>
+                    <span className="card-description subtle">
+                      {description}
