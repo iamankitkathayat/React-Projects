2a1855f767e183e3b8586d811cae27f2e372b0dc
Ankit Kathayat <93024110+iamankitkathayat@users.noreply.github.com>
2022-08-09 23:23:20 +0530

    Update useEffect.js

--- src/component/Hooks/useEffect.js
+++ src/component/Hooks/useEffect.js
@@ -1,11 +1,4 @@
-import React, { useState, useEffect } from "react";
-import "./style.css";
 
-const UseEffect = () => {
-  // const initialData = 15;
-  const [myNum, setMyNum] = useState(0);
-
-  useEffect(() => {
     document.title = `Chats(${myNum})`;
   });
 
