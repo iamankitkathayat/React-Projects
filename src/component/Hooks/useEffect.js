f9cd8ccc76857ab291f4854e5cea2c67643d61bb
Ankit Kathayat <93024110+iamankitkathayat@users.noreply.github.com>
2022-08-09 23:23:29 +0530

    Update useEffect.js

--- src/component/Hooks/useEffect.js
+++ src/component/Hooks/useEffect.js
@@ -1,16 +1,4 @@
 
-    document.title = `Chats(${myNum})`;
-  });
-
-  return (
-    <>
-      <div className="center_div">
-        <p>{myNum}</p>
-        <div class="button2" onClick={() => setMyNum(myNum + 1)}>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
           INCR
         </div>
       </div>
