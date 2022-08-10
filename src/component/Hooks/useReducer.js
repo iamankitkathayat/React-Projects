c2d5e7355f54505cd28936fbe294c07db481e1ba
Ankit Kathayat <93024110+iamankitkathayat@users.noreply.github.com>
2022-08-10 23:30:39 +0530

    Update useReducer.js

--- src/component/Hooks/useReducer.js
+++ src/component/Hooks/useReducer.js
@@ -1,21 +1,4 @@
 
-  }
-  return state;
-};
-
-const UseReducer = () => {
-  // const initialData = 15;
-  //   const [myNum, setMyNum] = React.useState(0);
-  const intialData = 10;
-  const [state, dispatch] = useReducer(reducer, intialData);
-
-  return (
-    <>
-      <div className="center_div">
-        <p>{state}</p>
-        <div class="button2" onClick={() => dispatch({ type: "INCR" })}>
-          <span></span>
-          <span></span>
           <span></span>
           <span></span>
           INCR
