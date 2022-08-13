aa7ac63f57875be5703c425fe8c6662ec2d05d5c
Ankit Kathayat <93024110+iamankitkathayat@users.noreply.github.com>
2022-08-13 23:52:45 +0530

    Update Restaurant.js

--- src/component/Basics/Restaurant.js
+++ src/component/Basics/Restaurant.js
@@ -1,10 +1,4 @@
-import React, { useState } from "react";
-import "./style.css";
-import Menu from "./menuApi.js";
-import MenuCard from "./MenuCard";
-import Navbar from "./Navbar";
 
-const uniqueList = [
   ...new Set(
     Menu.map((curElem) => {
       return curElem.category;
