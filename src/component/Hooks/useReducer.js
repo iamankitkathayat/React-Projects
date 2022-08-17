1c875d6aea744d4f38f0f90e2b73779ecd52858b
Ankit Kathayat <93024110+iamankitkathayat@users.noreply.github.com>
2022-08-17 23:48:01 +0530

    Update useReducer.js

--- src/component/Hooks/useReducer.js
+++ src/component/Hooks/useReducer.js
@@ -1,24 +1,3 @@
 
-    <>
-      <div className="center_div">
-        <p>{state}</p>
-        <div class="button2" onClick={() => dispatch({ type: "INCR" })}>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          INCR
-        </div>
-        <div class="button2" onClick={() => dispatch({ type: "DECR" })}>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          DECR
-        </div>
-      </div>
-    </>
-  );
-};
 
 export default UseReducer;
